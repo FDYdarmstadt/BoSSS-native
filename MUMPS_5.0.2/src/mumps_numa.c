5cf470028f5cfc69707143e354a86de196ed8a95
Jens Weber <weber@fdy.tu-darmstadt.de>
2018-07-09 15:40:55 +0200

    example test successful

---
+++ MUMPS_5.0.2/src/mumps_numa.c
@@ -0,0 +1,18 @@
+/*
+ *
+ *  This file is part of MUMPS 5.0.2, released
+ *  on Fri Jul 15 09:12:54 UTC 2016
+ *
+ *
+ *  Copyright 1991-2016 CERFACS, CNRS, ENS Lyon, INP Toulouse, Inria,
+ *  University of Bordeaux.
+ *
+ *  This version of MUMPS is provided to you free of charge. It is
+ *  released under the CeCILL-C license:
+ *  http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ *
+ */
+int mumps_numa_return()
+{
+return 0;
+}
