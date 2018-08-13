142a1db9a0f8e6c04621672eac99c252106d1d87
Jens Weber <weber@fdy.tu-darmstadt.de>
2018-08-13 18:18:48 +0200

    2 >& 1

--- dmumps-config/MUMPS_build_libs.bat
+++ dmumps-config/MUMPS_build_libs.bat
@@ -32,7 +32,7 @@ ECHO ========================
 ECHO clean up MUMPS directory
 ECHO ========================
 
-CALL make clean 2>nul
+CALL make clean 2 >& 1
 DEL Makefile.inc
 
 IF %MUMPS_TYPE%==SEQ (
