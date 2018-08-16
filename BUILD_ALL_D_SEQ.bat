fc2a8eff0f420be74fb7937a07ebde530b623dfb
User for Jenkins CI Server <JenkinsCI@fdy.maschinenbau.tu-darmstadt.de>
2018-08-16 16:58:44 +0200

    clean run

--- BUILD_ALL_D_SEQ.bat
+++ BUILD_ALL_D_SEQ.bat
@@ -4,21 +4,21 @@ IF DEFINED JENKINS (
 set "PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\cygwin64\bin;"
 set "WORKINGDIR=C:\Program Files (x86)\Jenkins\jobs\BoSSS-native\workspace"
 :: specify paths for VS-projects, plz edit if necessary:
-set "MKL_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2018.3.210\windows"
-set "MKL_LIB_DIR=%MKL_DIR%\mkl\lib\intel64_win"
+set "MKL_DIR=%PROGRAMFILES(x86)%\IntelSWTools\compilers_and_libraries_2018.3.210\windows"
+set "MKL_LIB_DIR=%PROGRAMFILES(x86)%\IntelSWTools\compilers_and_libraries_2018.3.210\windows\mkl\lib\intel64_win"
 :: directory of libiomp5md.lib ...
-set "MKL_OPENMP_DIR=%MKL_DIR%\compiler\lib\intel64_win"
-set "MS_MPI_DIR=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64"
+set "MKL_OPENMP_DIR=%PROGRAMFILES(x86)%\IntelSWTools\compilers_and_libraries_2018.3.210\windows\compiler\lib\intel64_win"
+set "MS_MPI_DIR=%PROGRAMFILES(x86)%\Microsoft SDKs\MPI\Lib\x64"
 )
+
 IF DEFINED HOME (
 set "PATH=%PATH%C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\cygwin\bin;"
 set WORKINGDIR=C:\BoSSS-native
-set "MKL_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2018.3.210\windows"
-::set "MKL_LIB_DIR=%MKL_DIR%\mkl\lib\intel64_win"
-set "MKL_LIB_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2018.3.210\windows\mkl\lib\intel64_win"
+set "MKL_DIR=%PROGRAMFILES(x86)%\IntelSWTools\compilers_and_libraries_2018.3.210\windows"
+set "MKL_LIB_DIR=%PROGRAMFILES(x86)%\IntelSWTools\compilers_and_libraries_2018.3.210\windows\mkl\lib\intel64_win"
 :: directory of libiomp5md.lib ...
-set "MKL_OPENMP_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2018.3.210\windows\compiler\lib\intel64_win"
-set "MS_MPI_DIR=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64"
+set "MKL_OPENMP_DIR=%PROGRAMFILES(x86)%\IntelSWTools\compilers_and_libraries_2018.3.210\windows\compiler\lib\intel64_win"
+set "MS_MPI_DIR=%PROGRAMFILES(x86)%\Microsoft SDKs\MPI\Lib\x64"
 )
 
 :: Build composition ::
