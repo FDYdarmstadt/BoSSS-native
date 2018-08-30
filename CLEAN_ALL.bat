eb2da9fbe7d5ced20b854711df8f06dc441299b5
User for Jenkins CI Server <JenkinsCI@fdy.maschinenbau.tu-darmstadt.de>
2018-08-30 14:21:47 +0200

    HYPRE finished, testing on JENKINS

--- CLEAN_ALL.bat
+++ CLEAN_ALL.bat
@@ -1,9 +1,5 @@
 @echo off
 :: Choose PLATFORM
-IF DEFINED JTEST (
-set "PATH=%PATH%C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\cygwin64\bin;"
-set WORKINGDIR=C:\Users\JenkinsCI\test\workspace
-)
 IF DEFINED JENKINS (
 set "PATH=%PATH%C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\cygwin64\bin;"
 set "WORKINGDIR=C:\Program Files (x86)\Jenkins\jobs\BoSSS-native\workspace"
@@ -33,13 +29,13 @@ cd "%PARDISO_BUILD%" >nul 2>&1
 rmdir x64 /s /q >nul 2>&1
 echo done.
 
-SET /P var= clean up MUMPS directory ... <nul
+SET /P var= clean MUMPS directory ... <nul
 CD "%MUMPS_THIRDPARTY%" >nul 2>&1
 CALL make clean >nul 2>&1
 DEL Makefile.inc >nul 2>&1
 echo done.
 
-SET /P var= clean up MUMPS-VS directory ... <nul
+SET /P var= clean MUMPS-VS directory ... <nul
 rmdir "%MUMPS_VSPROJECT%\x64" /s /q >nul 2>&1
 echo done.
 
@@ -60,4 +56,6 @@ rmdir "HYPRE.dir" /s /q >nul 2>&1
 rmdir Release /s /q >nul 2>&1
 echo done.
 
-cd %~dp0
+cd %WORKINGDIR%
+
+del /q "PropertySheet.props" >nul 2>&1
