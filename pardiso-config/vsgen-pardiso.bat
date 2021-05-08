dd08ece14984aee081316581e8eaa9e2859db340
Jenkinsci <Jenkinsci@FDYGITRUNNER>
2021-05-08 06:07:44 +0200

    PARDISO: seq and opm

--- pardiso-config/vsgen-pardiso.bat
+++ pardiso-config/vsgen-pardiso.bat
@@ -4,6 +4,8 @@ ECHO ^>^>^>^>^>^>PARDISO BUILD^>^>^>^>^>^>
 ECHO =======================
 ECHO.
 
+::echo checkpoint4: %PARDISO_TYPE%
+
 ::check if all necessary variables are set
 set "ErrorInput="
 IF NOT DEFINED WORKINGDIR set ErrorInput=1
@@ -34,20 +36,25 @@ for %%i in (*.sln) do set SLN_NAME=%%~ni
 SET PLATFORM=x64
 SET CONFIG=Release
 
+::echo checkpoint5: %PARDISO_TYPE%
+
 ::devenv PARDISO-seq.vcxproj /Build "Release|x64"
 ::devenv PARDISO-openMP.vcxproj /Build "Release|x64"
+set "DLL="
 IF %PARDISO_TYPE%==SEQ (
 CALL msbuild /property:Configuration=%CONFIG% /property:Platform=%PLATFORM% /property:SolutionName=%SLN_NAME% PARDISO-seq.vcxproj
+set DLL=PARDISO_seq.dll
 )
 IF %PARDISO_TYPE%==OPENMP (
 CALL msbuild /property:Configuration=%CONFIG% /property:Platform=%PLATFORM% /property:SolutionName=%SLN_NAME% PARDISO-openMP.vcxproj
+set DLL=PARDISO_omp.dll
 )
 ::check if build was successful
 
 CD "%PARDISO_BUILD%\%PLATFORM%\%CONFIG%"
 
 set "CHECK="
-IF EXIST PARDISO.dll SET CHECK=1
+IF EXIST %DLL% SET CHECK=1
 IF DEFINED CHECK (
 ECHO Build successful
 ) ELSE (
