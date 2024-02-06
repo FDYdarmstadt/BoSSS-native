ffd1d22f21d193c74873f3983ca2fab8841890c8
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2024-02-06 12:30:36 +0100

    converting to MKL 2024

--- SET_PATHS.bat
+++ SET_PATHS.bat
@@ -5,22 +5,23 @@ set ERRORS=0
 
 Echo Searching for executable, inculde and library directories ...
 echo.
-set "Intel_Version=2019.1.144"
+set "Intel_Version=2024.0"
 set "Executables=cmake make msbuild ifort link"	
 
 for /F %%E in ("%Executables%") do set "path_%%E="
 
-for /F "tokens=*" %%I IN ('dir /s /b "c:\Program Files (x86)\Microsoft Visual Studio\" ^| find /i "bin\cmake.exe" ^| find /i /v ".config"') do (
+for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files\Microsoft Visual Studio\2022\" ^| find /i "bin\cmake.exe" ^| find /i /v ".config"') do (
 set "a=%%~fI"
 set "path_cmake=!a:\cmake.exe=!"
 )
 
-for /F "tokens=*" %%I IN ('dir /s /b "c:\Program Files (x86)\Microsoft Visual Studio\" ^| find /i "Hostx64\x64\link.exe" ^| find /i /v ".config"') do (
+for /F "tokens=*" %%I IN ('dir /s /b "c:\Program Files\Microsoft Visual Studio\2022\" ^| find /i "Hostx64\x64\link.exe" ^| find /i /v ".config"') do (
 set "a=%%~fI"
 set "path_link=!a:\link.exe=!"
 )
 
-for /F "tokens=*" %%I IN ('dir /s /b "c:\Program Files (x86)\Microsoft Visual Studio\" ^| find /i "Bin\MSBuild.exe" ^| find /i /v ".config"') do (
+
+for /F "tokens=*" %%I IN ('dir /s /b "c:\Program Files\Microsoft Visual Studio\2022\" ^| find /i "Bin\MSBuild.exe" ^| find /i /v ".config"') do (
 set "a=%%~fI"
 set "path_msbuild=!a:\msbuild.exe=!"
 )
@@ -30,14 +31,16 @@ set "a=%%~fI"
 set "path_make=!a:\make.exe=!"
 )
 
-for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files (x86)\IntelSWTools\" ^| find /i "windows\bin\intel64\ifort.exe" ^| find /i "%Intel_Version%" ^| find /i /v ".config"') do (
-set "a=%%~fI"
-set "path_ifort=!a:\ifort.exe=!"
-)
+rem for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files (x86)\Intel\oneAPI\" ^| find /i "bin\ifort.exe" ^| find /i "%Intel_Version%" ^| find /i /v ".config"') do (
+rem set "a=%%~fI"
+rem set "path_ifort=!a:\ifort.exe=!"
+rem )
+set "path_ifort=C:\Program Files (x86)\Intel\oneAPI\compiler\2024.0\bin"
+
 
 set "target=time.h"
 if not defined AUXONE (
-for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files (x86)\Windows Kits\" ^| find /i "\%target%"') do (
+for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files (x86)\Windows Kits\10\" ^| find /i "\%target%"') do (
 set "a=%%~fI"
 set "AUXONE=!a:\%target%=!"
 )
@@ -50,7 +53,7 @@ echo !target! was found at AUXONE=!AUXONE!)
 
 set "target=vcruntime.h"
 if not defined AUXTWO (
-for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files (x86)\Microsoft Visual Studio\" ^| find /i "\%target%"') do (
+for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files\Microsoft Visual Studio\2022\" ^| find /i "\%target%"') do (
 set "a=%%~fI"
 set "AUXTWO=!a:\%target%=!"
 )
@@ -63,11 +66,12 @@ echo !target! was found at AUXTWO=!AUXTWO!)
 
 
 set "target=mkl_core.lib"
-if not defined MKL_LIB_DIR (
-for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files (x86)\IntelSWTools\" ^| find /i  "windows" ^| find /i "64" ^| find /i "\%target%" ^| find /i "%Intel_Version%"') do (
-set "a=%%~fI"
-set "MKL_LIB_DIR=!a:\%target%=!"
-)
+rem if not defined MKL_LIB_DIR (
+rem for /F "tokens=*" %%I IN ('dir /s /b "C:\Program Files (x86)\Intel\oneAPI\" ^| find /i "\%target%" ^| find /i "%Intel_Version%"') do (
+rem set "a=%%~fI"
+rem set "MKL_LIB_DIR=!a:\%target%=!"
+rem )
+set "MKL_LIB_DIR=C:\Program Files (x86)\Intel\oneAPI\mkl\2024.0\lib"
 if not defined MKL_LIB_DIR (
 echo path to !target! not found!
 set /a ERRORS=%ERRORS%+1
@@ -76,11 +80,12 @@ echo !target! was found at MKL_LIB_DIR=!MKL_LIB_DIR!)
 )
 
 set "target=libiomp5md.lib"
-if not defined MKL_OPENMP_DIR (
-for /F "tokens=*" %%I IN (' dir /s /b "C:\Program Files (x86)\IntelSWTools\" ^| find /i  "windows" ^| find /i "64" ^| find /i "\%target%" ^| find /i "%Intel_Version%"') do (
-set "a=%%~fI"
-set "MKL_OPENMP_DIR=!a:\%target%=!"
-)
+rem if not defined MKL_OPENMP_DIR (
+rem for /F "tokens=*" %%I IN (' dir /s /b "C:\Program Files (x86)\Intel\oneAPI\" ^| find /i  "lib" ^| find /i "\%target%" ^| find /i "%Intel_Version%"') do (
+rem set "a=%%~fI"
+rem set "MKL_OPENMP_DIR=!a:\%target%=!"
+rem )
+set "MKL_OPENMP_DIR=C:\Program Files (x86)\Intel\oneAPI\compiler\2024.0\lib"
 if not defined MKL_OPENMP_DIR (
 echo path to !target! not found!
 set /a ERRORS=%ERRORS%+1
