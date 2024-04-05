704d4574ed457d2f7ed684af1cb0a1583e18f918
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2024-04-05 13:39:14 +0200

    implemented OpenMP thread binding through Windows API, sinde Intel OMP occasionally asserts and kills the process

--- PARDISO/stdafx.h
+++ PARDISO/stdafx.h
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
 
