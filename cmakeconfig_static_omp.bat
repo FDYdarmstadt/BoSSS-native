43164df2c9826939d567e7e5f5f2859c9d289e99
Florian Kummer <kummer@fdy.tu-darmstadt.de>
2026-02-23 13:44:40 +0100

    added batch files to execute cmake config on windows

---
+++ cmakeconfig_static_omp.bat
@@ -0,0 +1,23 @@
+cmake --fresh ^
+      -S .\SuitSparse_7.12.2 -B .\SuitSparse_7.12.2_config_static_omp ^
+	  -G "Visual Studio 17 2022" -A x64 ^
+	  --log-level=VERBOSE ^
+	  -DBLA_VENDOR=Intel10_64lp ^
+	  -DBLAS_INCLUDE_DIRS="C:/Program Files (x86)/Intel/oneAPI/mkl/latest/include" ^
+	  -DBLAS_LIBRARIES="C:/Program Files (x86)/Intel/oneAPI/mkl/latest/lib/mkl_intel_lp64.lib;C:/Program Files (x86)/Intel/oneAPI/compiler/latest/lib/libiomp5md.lib;C:/Program Files (x86)/Intel/oneAPI/mkl/latest/lib/mkl_intel_thread.lib;C:/Program Files (x86)/Intel/oneAPI/mkl/latest/lib/mkl_core.lib" ^
+	  -DSUITESPARSE_USE_FORTRAN=OFF ^
+	  -DCMAKE_C_FLAGS="/I\"%MKLROOT%\include\"" ^
+      -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake ^
+	  -DVCPKG_TARGET_TRIPLET=x64-windows ^
+	  -DGMP_ROOT="C:\vcpkg\installed\x64-windows" ^
+	  -DGMP_INCLUDE_DIR="C:\vcpkg\installed\x64-windows\include" ^
+	  -DGMP_LIBRARY="C:\vcpkg\installed\x64-windows\lib\gmp.lib" ^
+	  -DMPFR_INCLUDE_DIR="C:\vcpkg\installed\x64-windows\include" ^
+	  -DMPFR_LIBRARY="C:\vcpkg\installed\x64-windows\lib\mpfr.lib" ^
+	  -DBUILD_SHARED_LIBS=OFF ^
+	  -DBUILD_STATIC_LIBS=ON ^
+	  -DSUITESPARSE_USE_OPENMP=ON
+
+::  dynamic
+:: -DBLAS_LIBRARIES="C:/Program Files (x86)/Intel/oneAPI/mkl/latest/lib/mkl_intel_lp64.lib;    C:/Program Files (x86)/Intel/oneAPI/mkl/latest/lib/libiomp5md.lib      C:/Program Files (x86)/Intel/oneAPI/compiler/latest/lib/mkl_intel_thread.lib;C:/Program Files (x86)/Intel/oneAPI/mkl/latest/lib/mkl_core.lib" ^
+ 	  
